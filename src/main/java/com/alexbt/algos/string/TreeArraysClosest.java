e34a0db8182241ac4203ee81c7a1df3e5efc777f
Alex Belisle Turcot <alex.belisleturcot@gmail.com>
2017-01-05 10:49:18 -0500

    fix warning

--- src/main/java/com/alexbt/algos/string/TreeArraysClosest.java
+++ src/main/java/com/alexbt/algos/string/TreeArraysClosest.java
@@ -4,6 +4,6 @@ public class TreeArraysClosest {
 
     // http://www.geeksforgeeks.org/find-three-closest-elements-from-given-three-sorted-arrays/
     public static void closestMatch(int[] arr1, int[] arr2, int[] arr3) {
-        int i = 0, j = 0, k = 0;
+        // int i = 0, j = 0, k = 0;
     }
 }
